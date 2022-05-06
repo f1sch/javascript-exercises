9e918f2487a42586e0420022d12f0364e1941510
Anders Fritz Fischer <Anders.Fischer1@googlemail.com>
2022-05-06 15:47:34 +0200

    Solve: 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+/*
+// longer code, faster? execution
+const reverseString = function(oldWord) {
+    let newWord = '';
+    for (let i = oldWord.length-1; i >= 0; i--) newWord += oldWord[i];
+    return newWord;
 };
+*/
+const reverseString = function(oldWord) {
+    return oldWord.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = reverseString;
