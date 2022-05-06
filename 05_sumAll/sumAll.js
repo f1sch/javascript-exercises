d10ca8830b2cea36e260b6331edb49bae6cdadb6
Anders Fritz Fischer <Anders.Fischer1@googlemail.com>
2022-05-06 23:35:34 +0200

    Solve: 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) return 'ERROR';
+    else if (!Number.isInteger(start) || !Number.isInteger(end)) return 'ERROR';
+    else if (start > end) {
+        const tmp = start;
+        start = end;
+        end = tmp;
+    }
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
