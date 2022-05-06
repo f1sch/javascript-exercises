adb14efed90611edec513f6f8bacdc57c64dfa5d
Anders Fritz Fischer <Anders.Fischer1@googlemail.com>
2022-05-07 00:02:01 +0200

    Solve: 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  // [°C] = ([°F] - 32) * (5 / 9)
+  return Math.round((temp - 32) * (5/9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  // [°F] = [°C] * (5/9) + 32
+  return Math.round(((temp * (9/5) + 32)) * 10) / 10;
 };
 
 // Do not edit below this line
