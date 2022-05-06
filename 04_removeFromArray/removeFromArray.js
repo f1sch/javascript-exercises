f6df3bc3f36cf515e33f577d80c75cf2de908981
Anders Fritz Fischer <Anders.Fischer1@googlemail.com>
2022-05-06 23:14:43 +0200

    Solve: 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let newArray = [];
+    const array = args[0];
+    array.forEach((item) => {
+        if(!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
+
+// var removeFromArray = function(...args) {
+//   const array = args[0]
+//   return array.filter(val => !args.includes(val))
+// }
+//
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
